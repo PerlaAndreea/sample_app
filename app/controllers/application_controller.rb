e8269d0af192358da9b955ba5bd1dfb48a35ef4a
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-02 21:54:57 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ApplicationHelper
    def hello
     render html: "hello, world!"
-  end
+   end
 end
