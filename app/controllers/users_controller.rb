c38bdbb586bbf17c9bec7be067127567a76cb345
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-29 11:40:50 +0000

    Add password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-      before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     before_action :correct_user,   only: [:edit, :update]
     before_action :admin_user,     only: :destroy
     
     def index
-    @users = User.where(activated: FILL_IN).paginate(page: 
-params[:page])
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end 
   def show
     @user = User.find(params[:id])
