3ab13fb80908e1d1c185ff8138fdcc6f28b333d6
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-31 12:00:13 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,4 +40,13 @@ password_confirmation: "password",
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
+   test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+ end
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+ end
 end
